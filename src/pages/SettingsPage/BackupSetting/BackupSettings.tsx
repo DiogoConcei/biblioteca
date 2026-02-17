0462c5535eff224a4cfb88f0fee3d64802d27133
DiogoConcei <diogooliveiraspot@gmail.com>
2026-02-17 17:50:06 -0300

    pause branch

--- src/pages/SettingsPage/BackupSetting/BackupSettings.tsx
+++ src/pages/SettingsPage/BackupSetting/BackupSettings.tsx
@@ -1,5 +1,12 @@
+import Select from 'react-select';
 import styles from './BackupSettings.module.scss';
 
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
 export default function BackupSettings() {
-  return <p>Sobre o backup</p>;
+  return <section></section>;
 }
